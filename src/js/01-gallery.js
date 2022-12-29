2072c4e13dc3b0cf43d1110a38536271bc651a92
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2022-12-29 15:11:00 +0100

    zadanie 1,2,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
+galleryItems.forEach(function(element) {
+    const itemOfElement = document.createElement("li");
+    const linkOfElement = document.createElement("a");
+    linkOfElement.classList.add("gallery__item");
+    linkOfElement.setAttribute("href", element.original);
+    const imageOfGallery = document.createElement("img");
+    imageOfGallery.classList.add("gallery__image");
+    imageOfGallery.setAttribute("src", element.preview);
+    imageOfGallery.setAttribute("alt", element.description);
+    gallery.append(itemOfElement);
+    itemOfElement.append(linkOfElement);
+    linkOfElement.append(imageOfGallery);
+    });
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+const itemOfGallery = document.querySelectorAll("li");
+itemOfGallery.forEach((element) => {
+    element.style.listStyleType = "none";
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
-
+import _ from 'lodash';
+const form = document.querySelector(".feedback-form");
+const inputEmail = document.querySelector("input");
+const inputMessage = document.querySelector("textarea");
+const STORAGE_KEY = "feedback-form-state";
+form.addEventListener("input", _.throttle(() => {
+ const { email, message } = form.elements;
+    let inputsForm = {
+    email: email.value,
+    message: message.value,
+    }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputsForm));
+},500));
+const currentValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+form.addEventListener("submit", (event)=> {
+   event.preventDefault();
+   console.log(currentValues);
+   localStorage.clear();
+   form.reset();
+});
+window.addEventListener("load", () => {
+   inputEmail.value = currentValues.email || "";
+   inputMessage.value = currentValues.message || "";
+}); 
