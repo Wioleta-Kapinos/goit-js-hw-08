e5daca4bf1891a057338ab12976d3ad411c7583e
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2022-12-30 16:37:54 +0100

    zmiana w submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,24 +3,42 @@ const form = document.querySelector(".feedback-form");
 const inputEmail = document.querySelector("input");
 const inputMessage = document.querySelector("textarea");
 const STORAGE_KEY = "feedback-form-state";
+let inputsForm
 form.addEventListener("input", _.throttle(() => {
  const { email, message } = form.elements;
-    let inputsForm = {
+    inputsForm = {
     email: email.value,
     message: message.value,
     }
   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputsForm));
 },500));
 const currentValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
-form.addEventListener("submit", (event)=> {
-   event.preventDefault();
-   console.log(currentValues);
-   localStorage.removeItem(STORAGE_KEY);
-   form.reset();
-});
 window.addEventListener("load", () => {
    if(currentValues !== null) {
       inputEmail.value = currentValues.email;
       inputMessage.value = currentValues.message;
    };
 }); 
+form.addEventListener("submit", (event)=> {
+   event.preventDefault();
+   // if(!localStorage.getItem(STORAGE_KEY)) {
+   //    inputsForm = {
+   //       email: "",
+   //      message: "",
+   //    }
+   //  console.log(inputsForm);
+   // } else {
+   //    console.log(currentValues);
+   // };
+   if(currentValues !==null) {
+      console.log(currentValues);
+    } else {
+       inputsForm = {
+          email: "",
+          message: "",
+       }
+       console.log(inputsForm);
+    };
+   localStorage.removeItem(STORAGE_KEY);
+   form.reset();
+});
