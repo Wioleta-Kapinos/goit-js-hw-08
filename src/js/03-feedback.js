1b9035bd16cdde390fec4798ff8097c2a9cbb48d
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2022-12-30 20:41:17 +0100

    korekta zadania 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,42 +3,27 @@ const form = document.querySelector(".feedback-form");
 const inputEmail = document.querySelector("input");
 const inputMessage = document.querySelector("textarea");
 const STORAGE_KEY = "feedback-form-state";
-let inputsForm
+
 form.addEventListener("input", _.throttle(() => {
  const { email, message } = form.elements;
-    inputsForm = {
-    email: email.value,
-    message: message.value,
-    }
+   inputsForm.email = email.value,
+   inputsForm.message = message.value,
   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputsForm));
 },500));
-const currentValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+let inputsForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+inputsForm ? "" : (inputsForm = { email: '', message: '' });
 window.addEventListener("load", () => {
-   if(currentValues !== null) {
-      inputEmail.value = currentValues.email;
-      inputMessage.value = currentValues.message;
-   };
+   inputEmail.value = inputsForm.email;
+   inputMessage.value = inputsForm.message;
 }); 
+
 form.addEventListener("submit", (event)=> {
-   event.preventDefault();
-   // if(!localStorage.getItem(STORAGE_KEY)) {
-   //    inputsForm = {
-   //       email: "",
-   //      message: "",
-   //    }
-   //  console.log(inputsForm);
-   // } else {
-   //    console.log(currentValues);
-   // };
-   if(currentValues !==null) {
-      console.log(currentValues);
-    } else {
-       inputsForm = {
-          email: "",
-          message: "",
-       };
-       console.log(inputsForm);
-    };
+   event.preventDefault(); 
+   if(inputsForm.email === '' || inputsForm.message === '') {
+      return alert("Wypełnij wszystkie pola !!!");
+   }
+   console.log(inputsForm);
    localStorage.removeItem(STORAGE_KEY);
    form.reset();
 });
